1df44c22452ca5cde9ab918cf5fc1c5c0f836be3
Akshay Narayan <akshayn@mit.edu>
2018-04-26 14:17:13 -0400

    subsample experiment data before plotting

---
+++ parse/sampleCwndEvo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import sys
+import itertools
+import numpy as np
+import random
+
+def read(fn):
+    with open(fn, 'r') as f:
+        for line in f:
+            sp = line.split()
+            if len(sp) != 7:
+                continue
+            yield tuple(sp)
+
+def groupByIter(ls):
+    ixCol = 3
+    iyCol = 4
+    sls = sorted(ls,key=lambda x:(x[2], x[3], x[4]))
+    return itertools.groupby(sls, key=lambda x:(x[2], x[3], x[4]))
+
+if __name__ == '__main__':
+    fn = sys.argv[1]
+    sample = int(sys.argv[2])
+    print("Algorithm Impl Scenario IterationX IterationY Time Cwnd")
+    ls = read(fn)
+    next(ls) # drop first line
+    for _, it in groupByIter(ls):
+        line = list(it)
+        if len(line) > sample:
+            line = random.sample(line, sample)
+        for l in line:
+            print(*l)
--- scripts/run-exp.py
+++ scripts/run-exp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import os
 import sys
 import subprocess
 import time
@@ -7,57 +8,70 @@ import threading
 
 from start_ccp import start as ccp_start
 
-dest = sys.argv[1]
-iters = int(sys.argv[2])
-dur = sys.argv[3]
-scenarios = ('fixed', 'cell', 'drop')
-algs = {
-    'reno': './portus/ccp_generic_cong_avoid/target/debug/reno',
-    'cubic': './portus/ccp_generic_cong_avoid/target/debug/cubic',
-}
+def setup(dest):
+    print("setup")
+    print("=========================")
+    subprocess.run('./scripts/setup.sh', shell=True)
 
-ccp_exps = [(a, 'ccp', '{}-ccp'.format(a)) for a in algs]
-kernel_exps = [(a, a, '{}-kernel'.format(a)) for a in algs]
-exps = ccp_exps + kernel_exps
+    print(dest)
+    print("=========================")
+    for e in exps:
+        print(e)
 
-print("setup")
-print("=========================")
-subprocess.run('./scripts/setup.sh', shell=True)
+    subprocess.run('mkdir -p {}'.format(dest), shell=True)
+    subprocess.Popen('./scripts/run-iperf-server.sh > {0}/iperf-server.log'.format(dest), shell=True)
 
-print(dest)
-print("=========================")
-for e in exps:
-    print(e)
+def exps(exps, dest, iters, dur, scenarios):
+    for alg, sockopt, name in exps:
+        for trace in scenarios:
+            for i in range(iters):
+                if sockopt == 'ccp':
+                    subprocess.run('sudo killall reno cubic bbr 2> /dev/null', shell=True)
+                    threading.Thread(target=ccp_start, args=(dest, alg, '{}-{}-{}'.format(name, trace, i)), daemon=True).start()
+                    time.sleep(1)
 
-subprocess.run('mkdir -p {}'.format(dest), shell=True)
-subprocess.Popen('./scripts/run-iperf-server.sh > {0}/iperf-server.log'.format(dest), shell=True)
+                if trace == 'fixed':
+                    subprocess.run(['bash', './scripts/run-fixed-exp.sh', dest, sockopt, "{}-{}".format(name, i), dur])
+                elif trace == 'cell':
+                    subprocess.run(['bash', './scripts/run-cell-exp.sh', dest, sockopt, "{}-{}".format(name, i), dur])
+                elif trace == 'drop':
+                    subprocess.run(['bash', './scripts/run-drop-exp.sh', dest, sockopt, "{}-{}".format(name, i), dur])
+                else:
+                    print('unknown', trace)
+                    break
 
-for alg, sockopt, name in exps:
-    for trace in scenarios:
-        for i in range(iters):
-            if sockopt == 'ccp':
-                subprocess.run('sudo killall reno cubic bbr 2> /dev/null', shell=True)
-                threading.Thread(target=ccp_start, args=(dest, alg, '{}-{}-{}'.format(name, trace, i)), daemon=True).start()
-                time.sleep(1)
+                if sockopt == 'ccp':
+                    subprocess.run('sudo killall ccp 2> /dev/null', shell=True)
+                    time.sleep(1)
 
-            if trace == 'fixed':
-                subprocess.run(['bash', './scripts/run-fixed-exp.sh', dest, sockopt, "{}-{}".format(name, i), dur])
-            elif trace == 'cell':
-                subprocess.run(['bash', './scripts/run-cell-exp.sh', dest, sockopt, "{}-{}".format(name, i), dur])
-            elif trace == 'drop':
-                subprocess.run(['bash', './scripts/run-drop-exp.sh', dest, sockopt, "{}-{}".format(name, i), dur])
-            else:
-                print('unknown', trace)
-                break
+    subprocess.run('sudo killall iperf 2> /dev/null', shell=True)
 
-            if sockopt == 'ccp':
-                subprocess.run('sudo killall ccp 2> /dev/null', shell=True)
-                time.sleep(1)
+def plot(dest, algs):
+    print("Cwnd Evolution")
+    print("=========================")
+    subprocess.run('python3 parse/parseCwndEvo.py {0}/* > {0}/cwndevo.log'.format(dest), shell=True)
+    subprocess.run('python3 parse/sampleCwndEvo.py {0}/cwndevo.log 1000 > {0}/cwndevo-subsampled.log'.format(dest), shell=True)
+    for alg in algs:
+        subprocess.run('./plot/cwnd-evo.r {0}/cwndevo-subsampled.log {1} {0}/{1}-cwndevo.pdf'.format(dest, alg), shell=True)
 
-subprocess.run('sudo killall iperf 2> /dev/null', shell=True)
+if __name__ == '__main__':
+    dest = sys.argv[1]
+    iters = int(sys.argv[2])
+    dur = sys.argv[3]
+    scenarios = ('fixed', 'cell', 'drop')
+    algs = {
+        'reno': './portus/ccp_generic_cong_avoid/target/debug/reno',
+        'cubic': './portus/ccp_generic_cong_avoid/target/debug/cubic',
+    }
 
-print("Cwnd Evolution")
-print("=========================")
-subprocess.run('python3 parse/parseCwndEvo.py {0}/* > {0}/cwndevo.log'.format(dest), shell=True)
-for alg in algs:
-    subprocess.run('./plot/cwnd-evo.r {0}/cwndevo.log {1} {0}/{1}-cwndevo.pdf'.format(dest, alg), shell=True)
+    ccp_exps = [(a, 'ccp', '{}-ccp'.format(a)) for a in algs]
+    kernel_exps = [(a, a, '{}-kernel'.format(a)) for a in algs]
+
+    if not os.path.exists(dest):
+        print("Running Experiments")
+        print("=========================")
+        setup(dest)
+        exps(ccp_exps + kernel_exps, dest, iters, dur, scenarios)
+    else:
+        print("> Experiments already run, re-plotting")
+    plot(dest, algs)
